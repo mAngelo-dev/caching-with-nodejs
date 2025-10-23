57ed0f23f889ad1d2287d982eef62b0e194cfd2e
Miguel Angelo <m.angelodev@gmail.com>
2025-10-23 18:27:53 -0300

    try new method

--- app/main.js
+++ app/main.js
@@ -80,7 +80,7 @@ const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     // Now connection has a parsed data from RESP parser that knows which type is the data, we are treating PING and ECHO but the parser can throw errors for unknown types
     const parsedData = parseResp(data)
-    if (parsedData === "PING") {
+    if (parsedData[0].toUpperCase() === "PING") {
       connection.write("+PONG\r\n");
     } else {
       if (typeof parsedData === 'object' && parsedData[0] === "ECHO") {
