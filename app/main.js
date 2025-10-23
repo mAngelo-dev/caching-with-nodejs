de455f20abaecc1ab8603bddd3b7e2b3c3f33b47
Miguel Angelo <m.angelodev@gmail.com>
2025-10-23 16:22:50 -0300

    Try to establish ping > pong response

--- app/main.js
+++ app/main.js
@@ -2,6 +2,9 @@ const net = require("net");
 
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.on("PING", () => {
+    connection.write("PONG\r\n");
+  })
 });
 
 server.listen(6379, "127.0.0.1");
